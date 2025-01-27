55d91ad061af8822863bf74c4764602e025ef897
Adebanjo Fajemisin <marcusfajemisin@gmail.com>
2025-01-27 17:16:34 -0500

    edited .json

--- src/interfaces/CandidateInterface.tsx
+++ src/interfaces/CandidateInterface.tsx
@@ -1,14 +1,11 @@
 export interface Candidate {
-    login: string; // GitHub username
-    id: number; // Unique GitHub user ID
-    avatar_url: string; // URL of the avatar image
-    html_url: string; // URL to the GitHub profile
-    name?: string; // Full name of the user (optional, may be null)
-    company?: string; // Company of the user (optional, may be null)
-    location?: string; // Location of the user (optional, may be null)
-    email?: string; // Email address of the user (optional, may be null)
-    bio?: string; // Bio/description of the user (optional, may be null)
-    public_repos: number; // Number of public repositories
-    followers: number; // Number of followers
-    following: number; // Number of following
-  }
+    login: string; 
+    id: number; 
+    avatar_url: string; 
+    html_url: string; 
+    name?: string; 
+    company?: string; 
+    location?: string; 
+    email?: string; 
+    bio?: string; 
+};
